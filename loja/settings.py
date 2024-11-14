832eb03e3ed6736eaa1ec127162865e9e6254958
Ernane <nandy.eso@gmail.com>
2024-11-14 20:42:16 -0300

    update

--- loja/settings.py
+++ loja/settings.py
@@ -272,3 +272,11 @@ LOGGING = {
 #         },
 #     },
 # }
+
+# Configurações de email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'django.test.py@gmail.com'
+EMAIL_HOST_PASSWORD = 'swvurfmwqlrydjbj '  # Senha app 
--- perfil/urls.py
+++ perfil/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 app_name = 'perfil'
 
@@ -9,4 +11,12 @@ urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
+
+    # URLs para recuperação de senha
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('perfil:password_reset_done')), name="reset_password"),
+    path('reset_password_done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    
 ]
